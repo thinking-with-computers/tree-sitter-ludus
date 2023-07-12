621fa7ade201a08a6097c548d896f3948faab87b
Scott Richmond <s.richmond@utoronto.ca>
2023-07-11 23:11:15 -0400

    Make some real progress (this is remarkably easy)

--- grammar.js
+++ grammar.js
@@ -6,32 +6,49 @@ module.exports = grammar({
   rules: {
     source_file: $ => seq(
       optional($._terminator),
-      repeat1($._line),
-      optional($._expression)
+      repeat($._line),
+      $.expression,
+      optional($._terminator),
     ),
 
-    _line: $ => seq($._expression, $._terminator),
+    _line: $ => seq($.expression, $._terminator),
  
     _terminator: $ => repeat1(choice(
-      $.newline,
-      $.semicolon,
+      "\n",
+      ";",
     )),
 
-    newline: $ => "\n",
-
-    semicolon: $ => ";",
-
-    _expression: $ => choice(
-      $._literal,
+    expression: $ => choice(
+      $.literal,
       $.name,
+      $.if,
+      $._collection,
     ),
 
+    if: $ => seq(
+      "if",
+      $.expression,
+      repeat("\n"),
+      "then",
+      $.expression,
+      repeat("\n"),
+      "else",
+      $.expression
+    ),
+    
     name: $ => /[a-z][\w\-\!\?]*/,
 
     literal: $ => choice(
       $._atom,
       $.number,
       $.symbol,
+      $.string,
+    ),
+
+    string: $ => seq(
+      '"',
+      /[^\"]*/,
+      '"',
     ),
 
     _atom: $ => choice(
@@ -48,7 +65,84 @@ module.exports = grammar({
 
     number: $ => /\-?(([1-9]\d*)|0)(\.\d+)?/,
 
-    symbol: $ => seq(":", $.name),
+    symbol: $ => /:[a-z][\w\-\!\?]*/,
+
+    _collection: $ => choice(
+      $.tuple,
+      $.dict,
+      $.struct,
+      $.list,
+      $.set,
+    ),
+
+    tuple: $ => seq(
+      "(",
+      $._linear_collection,
+      ")",
+    ),
+
+    _tuple_term: $ => choice($.expression, $.splat),
+
+    splat: $ => seq("...", $.name),
+
+    _tuple_entry: $ => seq($._tuple_term, $._separator),
+
+    _separator: $ => repeat1(choice(",", "\n")),
+
+    _linear_collection: $ => seq( 
+      optional($._separator),
+      repeat($._tuple_entry),
+      $._tuple_term,
+      optional($._separator),
+    ),
+
+    list: $ => seq(
+      "[",
+      $._linear_collection,
+      "]",
+    ),
+
+    set: $ => seq(
+      "${",
+      $._linear_collection,
+      "}",
+    ),
+
+    dict: $ => seq(
+      "#{",
+      optional($._separator),
+      repeat($._dict_entry),
+      $._dict_term,
+      optional($._separator),
+      "}",
+    ),
+
+    _dict_term: $ => choice(
+      $.assoc,
+      $.name,
+      $.splat
+    ),
+
+    _dict_entry: $ => seq($._dict_term, $._separator),
+
+    assoc: $ => seq($.symbol, $.expression),
+
+    struct: $ => seq(
+      "@{",
+      optional($._separator),
+      repeat($._struct_entry),
+      $._struct_term,
+      optional($._separator),
+      "}",
+    ),
+
+    _struct_term: $ => choice(
+      $.assoc,
+      $.name,
+    ),
+
+    _struct_entry: $ => seq($._struct_term, $._separator),
+    
     
   }
 });
